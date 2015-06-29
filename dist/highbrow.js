76f4b6ace220ed7619275f027c7eeff4b9ca2e5e
Fleming Slone <fslone@gmail.com>
2015-06-29 18:09:54 -0400

    Re-organizing dir structure and adding some version checks to browser vendor check methods

--- dist/highbrow.js
+++ dist/highbrow.js
@@ -7,57 +7,165 @@
   })(this, function() {
     return highbrow;
   }, root = this || global, highbrow = {}, highbrow = {
-    alert: function(msg) {
-      return window.alert(msg);
-    },
-    assign: function() {
-      return window.location.assign();
-    },
-    atob: function(str) {
-      return window.atob(str);
-    },
-    back: function() {
-      return window.history.back();
-    },
-    blur: function() {
-      return window.blur();
-    },
-    btoa: function(str) {
-      return window.btoa(str);
-    },
-    close: function() {
-      return window.close();
-    },
-    confirm: function(msg) {
-      return window.confirm(msg);
-    },
-    forward: function() {
-      return window.history.forward();
-    },
-    go: function(loc) {
-      return window.history.go(loc);
+    uA: window.navigator.userAgent.toLowerCase(),
+    "do": {
+      alert: function(msg) {
+        return window.alert(msg);
+      },
+      assign: function() {
+        return window.location.assign();
+      },
+      atob: function(str) {
+        return window.atob(str);
+      },
+      back: function() {
+        return window.history.back();
+      },
+      blur: function() {
+        return window.blur();
+      },
+      btoa: function(str) {
+        return window.btoa(str);
+      },
+      close: function() {
+        return window.close();
+      },
+      confirm: function(msg) {
+        return window.confirm(msg);
+      },
+      forward: function() {
+        return window.history.forward();
+      },
+      go: function(loc) {
+        return window.history.go(loc);
+      },
+      prompt: function(msg, def) {
+        return window.prompt(msg, def);
+      },
+      reload: function() {
+        return window.location.reload();
+      },
+      replace: function() {
+        return window.location.replace();
+      },
+      stop: function() {
+        return window.stop();
+      }
     },
     is: {
-      chrome: function() {
-        return window.chrome != null;
+      android: function() {
+        return /android/i.test(highbrow.uA);
+      },
+      blackberry: function() {
+        return /blackberry/i.test(highbrow.uA) || /BB10/i.test(highbrow.uA);
+      },
+      chrome: function(v) {
+        var major, vEnd, vNum, vStart;
+        if (v != null) {
+          if (highbrow.is.chrome() === true) {
+            vStart = highbrow.uA.indexOf('chrome');
+            vEnd = highbrow.uA.indexOf(' ', vStart);
+            vNum = uA.substring(vStart, vEnd).split('/')[1];
+            major = vNum.split('.')[0];
+            return v === parseInt(major);
+          } else {
+            return false;
+          }
+        } else {
+          return window.chrome != null;
+        }
       },
       closed: function() {
         return window.closed;
       },
-      firefox: function() {
-        return highbrow.get.userAgentDowncase().indexOf('firefox') === !-1;
+      firefox: function(v) {
+        var major, vEnd, vStart;
+        if (v != null) {
+          if (highbrow.is.firefox() === true) {
+            vStart = highbrow.uA.indexOf('firefox') + 8;
+            vEnd = highbrow.uA.indexOf('.') + vStart;
+            major = highbrow.uA.substring(vStart, vEnd);
+            return version === parseInt(major);
+          } else {
+            return false;
+          }
+        } else {
+          return /firefox/i.test(highbrow.uA);
+        }
       },
       opera: function() {
         return highbrow.get.userAgentDowncase().indexOf('presto') === !-1;
       },
-      safari: function() {
-        return highbrow.get.userAgentDowncase().indexOf('chrome') === !-1 && highbrow.get.userAgentDowncase.indexOf('safari') === !-1;
-      },
-      ie: function() {
-        return (!version)(/msie/i.test(userAgent) || indexOf.call(window, 'ActiveXObject') >= 0);
+      ie: function(v) {
+        return !(typeof v === "function" ? v(/msie/i.test(highbrow.uA) || indexOf.call(window, 'ActiveXObject') >= 0) : void 0);
       },
       online: function() {
         return window.navigator.onLine;
+      },
+      ipad: function() {
+        return /ipad/i.test(highbrow.uA);
+      },
+      iphone: function(v) {
+        var vEnd, vNum, vStart;
+        if (v != null) {
+          if (highbrow.is.iphone()) {
+            vStart = highbrow.uA.indexOf('iphone os ');
+            vEnd = highbrow.uA.indexOf('_', vStart);
+            vNum = highbrow.uA.substring(vStart + 10, vEnd);
+            if (v === 1 && vStart === -1) {
+              return true;
+            } else {
+              return v === parseInt(vNum);
+            }
+          } else {
+            return false;
+          }
+        } else {
+          return /iphone/i.test(highbrow.uA);
+        }
+      },
+      ipod: function() {
+        return /ipod/i.test(highbrow.uA);
+      },
+      linux: function() {
+        return /linux/i.test(highbrow.get.appVersion);
+      },
+      mac: function() {
+        return /mac/i.test(highbrow.get.appVersion);
+      },
+      safari: function(v) {
+        var major, vEnd, vSlash, vStart;
+        if (v != null) {
+          vSlash = highbrow.uA.indexOf('version/');
+          if (vSlash === !-1) {
+            vStart = vSlash + 8;
+            vEnd = highbrow.uA.indexOf('.', vStart);
+            major = highbrow.uA.substring(vStart, vEnd);
+            return v === parseInt(major);
+          } else {
+            vStart = highbrow.uA.indexOf('safari/') + 7;
+            vEnd = highbrow.uA.indexOf('.', vStart);
+            major = parseInt(highbrow.uA.substring(vStart, vEnd));
+            if (v === 2 && major < 500 && major > 400) {
+              return true;
+            } else if (v === 1 && major < 400) {
+              return true;
+            } else {
+              return false;
+            }
+          }
+        } else {
+          return /safari/i.test(highbrow.uA) && /apple computer/i.test(highbrow.get.vendor);
+        }
+      },
+      silk: function() {
+        return /silk/i.test(highbrow.uA);
+      },
+      windows: function() {
+        return /win/i.test(highbrow.get.appVersion);
+      },
+      windowsPhone: function() {
+        return this.windows() && /phone/i.test(highbrow.uA);
       }
     },
     get: {
@@ -91,8 +199,13 @@
       language: function() {
         return window.navigator.language;
       },
-      location: function() {
-        return window.navigator.geoLocation;
+      geolocation: function() {
+        return window.navigator.geolocation;
+      },
+      maxTouchPoints: function() {
+        return function() {
+          return window.navigator.maxTouchPoints;
+        };
       },
       name: function() {
         return window.name;
@@ -111,6 +224,12 @@
       },
       port: function() {
         return window.location.port;
+      },
+      product: function() {
+        return window.navigator.product;
+      },
+      productSub: function() {
+        return window.navigator.productSub;
       },
       protocol: function() {
         return window.location.protocol;
@@ -133,23 +252,15 @@
       userAgentDowncase: function() {
         return window.navigator.userAgent.toLowerCase();
       },
+      vendor: function() {
+        return window.navigator.vendor;
+      },
+      vendorSub: function() {
+        return window.navigator.vendorSub;
+      },
       width: function() {
         return Math.max(document.documentElement['clientWidth'], document.body['scrollWidth'], document.documentElement['scrollWidth'], document.body['offsetWidth'], document.documentElement['offsetWidth']);
       }
-    },
-    not: function(fnc) {
-      return function() {
-        return !fnc.apply(null, Array.slice(arguments));
-      };
-    },
-    prompt: function(msg, def) {
-      return window.prompt(msg, def);
-    },
-    reload: function() {
-      return window.location.reload();
-    },
-    replace: function() {
-      return window.location.replace();
     },
     set: {
       cookie: function(name, value, daysTilExpire) {
@@ -166,9 +277,6 @@
       location: function(loc) {
         return window.location = loc;
       }
-    },
-    stop: function() {
-      return window.stop();
     }
   });
 
--- src/highbrow.coffee
+++ src/highbrow.coffee
@@ -9,26 +9,100 @@
 	->(highbrow)
 	root = @ || global
 	highbrow = {} 
-	highbrow =  
-		alert: (msg) -> window.alert(msg)
-		assign: -> window.location.assign()
-		atob: (str) -> window.atob(str)
-		back: -> window.history.back()
-		blur: -> window.blur()
-		btoa: (str) -> window.btoa(str)
-		close: -> window.close()
-		confirm: (msg) -> window.confirm(msg)
-		forward: -> window.history.forward()
-		go: (loc) -> window.history.go(loc)
+	highbrow =
+		uA: window.navigator.userAgent.toLowerCase()
+		do: 
+			alert: (msg) -> window.alert(msg)
+			assign: -> window.location.assign()
+			atob: (str) -> window.atob(str)
+			back: -> window.history.back()
+			blur: -> window.blur()
+			btoa: (str) -> window.btoa(str)
+			close: -> window.close()
+			confirm: (msg) -> window.confirm(msg)
+			forward: -> window.history.forward()
+			go: (loc) -> window.history.go(loc)
+			prompt: (msg, def) -> window.prompt(msg, def)
+			reload: -> window.location.reload()
+			replace: -> window.location.replace()
+			stop: -> window.stop()
 		is:
-			chrome: -> window.chrome?
+			android: -> /android/i.test(highbrow.uA)
+			blackberry: -> /blackberry/i.test(highbrow.uA) || /BB10/i.test(highbrow.uA)
+			chrome: (v) ->
+				if v? 
+					if highbrow.is.chrome() is true 
+						vStart = highbrow.uA.indexOf('chrome')
+						vEnd = highbrow.uA.indexOf(' ', vStart)
+						vNum = uA.substring(vStart, vEnd).split('/')[1]
+						major = vNum.split('.')[0]
+						v is parseInt major
+					else 
+						false
+				else
+					window.chrome?
 			closed: -> window.closed
-			firefox: -> highbrow.get.userAgentDowncase().indexOf('firefox') is not -1
+			firefox: (v) -> 
+				if v?
+					if highbrow.is.firefox() is true
+						vStart = highbrow.uA.indexOf('firefox') + 8
+						vEnd = highbrow.uA.indexOf('.') + vStart
+						major = highbrow.uA.substring(vStart, vEnd)
+						version is parseInt major
+					else 
+						false
+				else
+						/firefox/i.test(highbrow.uA)
 			opera: -> highbrow.get.userAgentDowncase().indexOf('presto') is not -1
-			safari: -> highbrow.get.userAgentDowncase().indexOf('chrome') is not -1 and highbrow.get.userAgentDowncase.indexOf('safari') is not -1
-			ie: -> (!version) /msie/i.test(userAgent) || 'ActiveXObject' in window
+			ie: (v) -> !v? /msie/i.test(highbrow.uA) || 'ActiveXObject' in window
 			online: -> window.navigator.onLine
+			ipad: -> /ipad/i.test(highbrow.uA)
+			iphone: (v) ->
+				if v?
+					if highbrow.is.iphone()
+						vStart = highbrow.uA.indexOf('iphone os ')
+						vEnd = highbrow.uA.indexOf('_', vStart)
+						vNum = highbrow.uA.substring(vStart + 10, vEnd)
+						#original iPhone doesn't have the os portion of the UA
+						if v is 1 and vStart is -1 
+							true
+						else return v is parseInt vNum
+					else
+						false
+				else
+					/iphone/i.test highbrow.uA
+			ipod: -> /ipod/i.test(highbrow.uA)
+			linux: -> /linux/i.test(highbrow.get.appVersion)
+			mac: -> /mac/i.test(highbrow.get.appVersion)
+			safari: (v) -> 
+				if v?
+					#safari 3+ includes a version section
+					vSlash = highbrow.uA.indexOf('version/')
+					if vSlash is not -1
+						vStart = vSlash + 8
+						vEnd = highbrow.uA.indexOf('.', vStart)
+						major = highbrow.uA.substring(vStart, vEnd)
+						v is parseInt major
+					#otherwise handle all the old UA string formats
+					else
+						vStart = highbrow.uA.indexOf('safari/') + 7
+						vEnd = highbrow.uA.indexOf('.', vStart)
+						major = parseInt highbrow.uA.substring(vStart, vEnd)
+						#safari 2 has a block of version numbers from 400-500
+						if v is 2 and major < 500 and major > 400 
+							true
+						#safari 1 has a block of version numbers less than 400
+						else if v is 1 and major < 400 
+							true
+						else
+							false
+				else
+					/safari/i.test(highbrow.uA) and /apple computer/i.test(highbrow.get.vendor)
+			silk: ->/silk/i.test(highbrow.uA)
+			windows: -> /win/i.test(highbrow.get.appVersion)
+			windowsPhone: -> @.windows() and /phone/i.test(highbrow.uA)
 		get:
+			# cookie:
 			appName: -> window.navigator.appName
 			appVersion: -> window.navigator.appVersion
 			hash: -> window.location.hash
@@ -39,13 +113,16 @@
 			innerHeight: -> window.innerHeight
 			innerWidth: -> window.innerWidth
 			language: -> window.navigator.language
-			location: -> window.navigator.geoLocation
+			geolocation: -> window.navigator.geolocation
+			maxTouchPoints: -> -> window.navigator.maxTouchPoints
 			name: -> window.name
 			opener: -> window.opener
 			origin: -> window.location.origin
 			pathname: -> window.location.pathname
 			platform: -> window.navigator.platform
 			port: -> window.location.port
+			product: -> window.navigator.product
+			productSub: -> window.navigator.productSub
 			protocol: -> window.location.protocol
 			referrer: -> document.referrer
 			search: -> window.location.search
@@ -53,11 +130,9 @@
 			url: -> window.document.URL
 			userAgent: -> window.navigator.userAgent
 			userAgentDowncase: -> window.navigator.userAgent.toLowerCase()
+			vendor: -> window.navigator.vendor
+			vendorSub: -> window.navigator.vendorSub
 			width: -> Math.max document.documentElement['clientWidth'], document.body['scrollWidth'], document.documentElement['scrollWidth'], document.body['offsetWidth'], document.documentElement['offsetWidth']
-		not: (fnc) -> -> not fnc.apply(null, Array.slice arguments)
-		prompt: (msg, def) -> window.prompt(msg, def)
-		reload: -> window.location.reload()
-		replace: -> window.location.replace()
 		set:
 			cookie: (name, value, daysTilExpire) -> 
 				exdate = new Date().setDate exdate.getDate() + daysTilExpire
@@ -68,5 +143,4 @@
 			title: (title) ->
 				document.title = title
 			location: (loc) -> window.location = loc
-		stop: -> window.stop()
 )
